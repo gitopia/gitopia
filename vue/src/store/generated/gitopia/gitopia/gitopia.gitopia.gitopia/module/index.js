eb83ad77c167ccafe173dfeca1d6cabc3ee20801
Hariom Verma <hariom@gitopia.org>
2021-07-28 15:31:53 +0530

    Generated files
    Signed-off-by: Hariom Verma <hariom@gitopia.org>

--- vue/src/store/generated/gitopia/gitopia/gitopia.gitopia.gitopia/module/index.js
+++ vue/src/store/generated/gitopia/gitopia/gitopia.gitopia.gitopia/module/index.js
@@ -2,51 +2,51 @@
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgDeleteBranch } from "./types/gitopia/tx";
-import { MsgDeleteRepository } from "./types/gitopia/tx";
-import { MsgUpdateIssueTitle } from "./types/gitopia/tx";
-import { MsgDeleteComment } from "./types/gitopia/tx";
-import { MsgCreateIssue } from "./types/gitopia/tx";
-import { MsgDeleteIssue } from "./types/gitopia/tx";
-import { MsgUpdateComment } from "./types/gitopia/tx";
-import { MsgCreateUser } from "./types/gitopia/tx";
-import { MsgCreateRepository } from "./types/gitopia/tx";
-import { MsgRenameRepository } from "./types/gitopia/tx";
-import { MsgToggleIssueState } from "./types/gitopia/tx";
-import { MsgDeleteWhois } from "./types/gitopia/tx";
-import { MsgDeleteUser } from "./types/gitopia/tx";
-import { MsgCreateComment } from "./types/gitopia/tx";
-import { MsgUpdateWhois } from "./types/gitopia/tx";
 import { MsgCreateBranch } from "./types/gitopia/tx";
-import { MsgUpdateUser } from "./types/gitopia/tx";
-import { MsgUpdateIssueDescription } from "./types/gitopia/tx";
-import { MsgSetWhois } from "./types/gitopia/tx";
+import { MsgRenameRepository } from "./types/gitopia/tx";
+import { MsgUpdateWhois } from "./types/gitopia/tx";
 import { MsgUpdateIssue } from "./types/gitopia/tx";
+import { MsgCreateRepository } from "./types/gitopia/tx";
+import { MsgDeleteRepository } from "./types/gitopia/tx";
 import { MsgUpdateRepository } from "./types/gitopia/tx";
+import { MsgCreateIssue } from "./types/gitopia/tx";
+import { MsgDeleteUser } from "./types/gitopia/tx";
+import { MsgUpdateComment } from "./types/gitopia/tx";
+import { MsgDeleteIssue } from "./types/gitopia/tx";
+import { MsgUpdateUser } from "./types/gitopia/tx";
+import { MsgDeleteWhois } from "./types/gitopia/tx";
+import { MsgUpdateIssueDescription } from "./types/gitopia/tx";
+import { MsgDeleteComment } from "./types/gitopia/tx";
+import { MsgCreateUser } from "./types/gitopia/tx";
+import { MsgUpdateIssueTitle } from "./types/gitopia/tx";
+import { MsgToggleIssueState } from "./types/gitopia/tx";
 import { MsgSetDefaultBranch } from "./types/gitopia/tx";
+import { MsgDeleteBranch } from "./types/gitopia/tx";
+import { MsgSetWhois } from "./types/gitopia/tx";
+import { MsgCreateComment } from "./types/gitopia/tx";
 const types = [
-    ["/gitopia.gitopia.gitopia.MsgDeleteBranch", MsgDeleteBranch],
-    ["/gitopia.gitopia.gitopia.MsgDeleteRepository", MsgDeleteRepository],
-    ["/gitopia.gitopia.gitopia.MsgUpdateIssueTitle", MsgUpdateIssueTitle],
-    ["/gitopia.gitopia.gitopia.MsgDeleteComment", MsgDeleteComment],
-    ["/gitopia.gitopia.gitopia.MsgCreateIssue", MsgCreateIssue],
-    ["/gitopia.gitopia.gitopia.MsgDeleteIssue", MsgDeleteIssue],
-    ["/gitopia.gitopia.gitopia.MsgUpdateComment", MsgUpdateComment],
-    ["/gitopia.gitopia.gitopia.MsgCreateUser", MsgCreateUser],
-    ["/gitopia.gitopia.gitopia.MsgCreateRepository", MsgCreateRepository],
-    ["/gitopia.gitopia.gitopia.MsgRenameRepository", MsgRenameRepository],
-    ["/gitopia.gitopia.gitopia.MsgToggleIssueState", MsgToggleIssueState],
-    ["/gitopia.gitopia.gitopia.MsgDeleteWhois", MsgDeleteWhois],
-    ["/gitopia.gitopia.gitopia.MsgDeleteUser", MsgDeleteUser],
-    ["/gitopia.gitopia.gitopia.MsgCreateComment", MsgCreateComment],
-    ["/gitopia.gitopia.gitopia.MsgUpdateWhois", MsgUpdateWhois],
     ["/gitopia.gitopia.gitopia.MsgCreateBranch", MsgCreateBranch],
-    ["/gitopia.gitopia.gitopia.MsgUpdateUser", MsgUpdateUser],
-    ["/gitopia.gitopia.gitopia.MsgUpdateIssueDescription", MsgUpdateIssueDescription],
-    ["/gitopia.gitopia.gitopia.MsgSetWhois", MsgSetWhois],
+    ["/gitopia.gitopia.gitopia.MsgRenameRepository", MsgRenameRepository],
+    ["/gitopia.gitopia.gitopia.MsgUpdateWhois", MsgUpdateWhois],
     ["/gitopia.gitopia.gitopia.MsgUpdateIssue", MsgUpdateIssue],
+    ["/gitopia.gitopia.gitopia.MsgCreateRepository", MsgCreateRepository],
+    ["/gitopia.gitopia.gitopia.MsgDeleteRepository", MsgDeleteRepository],
     ["/gitopia.gitopia.gitopia.MsgUpdateRepository", MsgUpdateRepository],
+    ["/gitopia.gitopia.gitopia.MsgCreateIssue", MsgCreateIssue],
+    ["/gitopia.gitopia.gitopia.MsgDeleteUser", MsgDeleteUser],
+    ["/gitopia.gitopia.gitopia.MsgUpdateComment", MsgUpdateComment],
+    ["/gitopia.gitopia.gitopia.MsgDeleteIssue", MsgDeleteIssue],
+    ["/gitopia.gitopia.gitopia.MsgUpdateUser", MsgUpdateUser],
+    ["/gitopia.gitopia.gitopia.MsgDeleteWhois", MsgDeleteWhois],
+    ["/gitopia.gitopia.gitopia.MsgUpdateIssueDescription", MsgUpdateIssueDescription],
+    ["/gitopia.gitopia.gitopia.MsgDeleteComment", MsgDeleteComment],
+    ["/gitopia.gitopia.gitopia.MsgCreateUser", MsgCreateUser],
+    ["/gitopia.gitopia.gitopia.MsgUpdateIssueTitle", MsgUpdateIssueTitle],
+    ["/gitopia.gitopia.gitopia.MsgToggleIssueState", MsgToggleIssueState],
     ["/gitopia.gitopia.gitopia.MsgSetDefaultBranch", MsgSetDefaultBranch],
+    ["/gitopia.gitopia.gitopia.MsgDeleteBranch", MsgDeleteBranch],
+    ["/gitopia.gitopia.gitopia.MsgSetWhois", MsgSetWhois],
+    ["/gitopia.gitopia.gitopia.MsgCreateComment", MsgCreateComment],
 ];
 export const MissingWalletError = new Error("wallet is required");
 const registry = new Registry(types);
@@ -61,28 +61,28 @@ const txClient = async (wallet, { addr: addr } = { addr: "http://localhost:26657
     const { address } = (await wallet.getAccounts())[0];
     return {
         signAndBroadcast: (msgs, { fee, memo } = { fee: defaultFee, memo: "" }) => client.signAndBroadcast(address, msgs, fee, memo),
-        msgDeleteBranch: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteBranch", value: data }),
-        msgDeleteRepository: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteRepository", value: data }),
-        msgUpdateIssueTitle: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateIssueTitle", value: data }),
-        msgDeleteComment: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteComment", value: data }),
-        msgCreateIssue: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateIssue", value: data }),
-        msgDeleteIssue: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteIssue", value: data }),
-        msgUpdateComment: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateComment", value: data }),
-        msgCreateUser: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateUser", value: data }),
-        msgCreateRepository: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateRepository", value: data }),
-        msgRenameRepository: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgRenameRepository", value: data }),
-        msgToggleIssueState: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgToggleIssueState", value: data }),
-        msgDeleteWhois: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteWhois", value: data }),
-        msgDeleteUser: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteUser", value: data }),
-        msgCreateComment: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateComment", value: data }),
-        msgUpdateWhois: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateWhois", value: data }),
         msgCreateBranch: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateBranch", value: data }),
-        msgUpdateUser: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateUser", value: data }),
-        msgUpdateIssueDescription: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateIssueDescription", value: data }),
-        msgSetWhois: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgSetWhois", value: data }),
+        msgRenameRepository: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgRenameRepository", value: data }),
+        msgUpdateWhois: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateWhois", value: data }),
         msgUpdateIssue: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateIssue", value: data }),
+        msgCreateRepository: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateRepository", value: data }),
+        msgDeleteRepository: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteRepository", value: data }),
         msgUpdateRepository: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateRepository", value: data }),
+        msgCreateIssue: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateIssue", value: data }),
+        msgDeleteUser: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteUser", value: data }),
+        msgUpdateComment: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateComment", value: data }),
+        msgDeleteIssue: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteIssue", value: data }),
+        msgUpdateUser: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateUser", value: data }),
+        msgDeleteWhois: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteWhois", value: data }),
+        msgUpdateIssueDescription: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateIssueDescription", value: data }),
+        msgDeleteComment: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteComment", value: data }),
+        msgCreateUser: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateUser", value: data }),
+        msgUpdateIssueTitle: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgUpdateIssueTitle", value: data }),
+        msgToggleIssueState: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgToggleIssueState", value: data }),
         msgSetDefaultBranch: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgSetDefaultBranch", value: data }),
+        msgDeleteBranch: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgDeleteBranch", value: data }),
+        msgSetWhois: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgSetWhois", value: data }),
+        msgCreateComment: (data) => ({ typeUrl: "/gitopia.gitopia.gitopia.MsgCreateComment", value: data }),
     };
 };
 const queryClient = async ({ addr: addr } = { addr: "http://localhost:1317" }) => {
