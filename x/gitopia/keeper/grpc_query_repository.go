0b1985454800727472234f0be2cde38a490d4ec0
Hariom Verma <hariom@gitopia.org>
2021-08-06 19:26:27 +0530

    Implemented grpc endpoint to query repository issue by Iid
    Signed-off-by: Hariom Verma <hariom@gitopia.org>

--- x/gitopia/keeper/grpc_query_repository.go
+++ x/gitopia/keeper/grpc_query_repository.go
@@ -58,6 +58,39 @@ func (k Keeper) Repository(c context.Context, req *types.QueryGetRepositoryReque
 	return &types.QueryGetRepositoryResponse{Repository: &repository}, nil
 }
 
+func (k Keeper) RepositoryIssue(c context.Context, req *types.QueryGetRepositoryIssueRequest) (*types.QueryGetRepositoryIssueResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	var user types.User
+	var repository types.Repository
+	var issue types.Issue
+	ctx := sdk.UnwrapSDKContext(c)
+
+	if !k.HasUser(ctx, req.UserId) {
+		return nil, sdkerrors.ErrKeyNotFound
+	}
+
+	userStore := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.UserKey))
+	userKey := []byte(types.UserKey + req.UserId)
+	k.cdc.UnmarshalBinaryBare(userStore.Get(userKey), &user)
+
+	if repositoryId, ok := user.RepositoryNames[req.RepositoryName]; ok {
+		repositoryStore := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.RepositoryKey))
+		k.cdc.MustUnmarshalBinaryBare(repositoryStore.Get(GetRepositoryIDBytes(repositoryId)), &repository)
+
+		if issueId, ok := repository.IssueIids[req.IssueIid]; ok {
+			issueStore := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.IssueKey))
+			k.cdc.MustUnmarshalBinaryBare(issueStore.Get(GetIssueIDBytes(issueId)), &issue)
+
+			return &types.QueryGetRepositoryIssueResponse{Issue: &issue}, nil
+		}
+	}
+
+	return nil, sdkerrors.ErrKeyNotFound
+}
+
 func (k Keeper) BranchAll(c context.Context, req *types.QueryGetAllBranchRequest) (*types.QueryGetAllBranchResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
