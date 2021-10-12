4d49702d04a18a3777addd18241e6cf0c102fbf2
Hariom Verma <hariom@gitopia.org>
2021-10-12 18:11:38 +0530

    Implement API to get issues by assignee
    Signed-off-by: Hariom Verma <hariom@gitopia.org>

--- x/gitopia/keeper/grpc_query_repository.go
+++ x/gitopia/keeper/grpc_query_repository.go
@@ -527,6 +527,23 @@ func PaginateAllRepositoryIssue(
 		totalIssueCount = uint64(len(issueBuffer))
 	}
 
+	if option.Assignee != "" {
+		var issueBuffer []*types.RepositoryIssue
+		for i := 0; uint64(i) < totalIssueCount; i++ {
+			var issue types.Issue
+			k.cdc.MustUnmarshal(issueStore.Get(GetRepositoryIDBytes(issues[uint64(i)].Id)), &issue)
+			if _, exists := utils.AssigneeExists(issue.Assignees, option.Assignee); exists {
+				repositoryIssue := types.RepositoryIssue{
+					Id:  issue.Id,
+					Iid: issue.Iid,
+				}
+				issueBuffer = append(issueBuffer, &repositoryIssue)
+			}
+		}
+		issues = issueBuffer
+		totalIssueCount = uint64(len(issueBuffer))
+	}
+
 	if option.State == types.Issue_CLOSED.String() {
 		var issueBuffer []*types.RepositoryIssue
 		for i := 0; uint64(i) < totalIssueCount; i++ {
