8e1b4708f8c9c7eecebdbb4716d90370d400e428
Hariom Verma <hariom@gitopia.org>
2021-12-08 03:07:54 +0530

    Test User Keeper

---
+++ x/gitopia/keeper/user_test.go
@@ -0,0 +1,55 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	keepertest "github.com/gitopia/gitopia/testutil/keeper"
+	"github.com/gitopia/gitopia/testutil/sample"
+	"github.com/gitopia/gitopia/x/gitopia/keeper"
+	"github.com/gitopia/gitopia/x/gitopia/types"
+	"github.com/stretchr/testify/require"
+)
+
+func createNUser(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.User {
+	items := make([]types.User, n)
+	for i := range items {
+		items[i].Creator = sample.AccAddress()
+		items[i].Id = uint64(i)
+		items[i].Creator = keeper.AppendUser(ctx, items[i])
+	}
+	return items
+}
+
+func TestUserGet(t *testing.T) {
+	keeper, ctx := keepertest.GitopiaKeeper(t)
+	items := createNUser(keeper, ctx, 10)
+	for _, item := range items {
+		got, found := keeper.GetUser(ctx, item.Creator)
+		require.True(t, found)
+		require.Equal(t, item, got)
+	}
+}
+
+func TestUserRemove(t *testing.T) {
+	keeper, ctx := keepertest.GitopiaKeeper(t)
+	items := createNUser(keeper, ctx, 10)
+	for _, item := range items {
+		keeper.RemoveUser(ctx, item.Creator)
+		_, found := keeper.GetUser(ctx, item.Creator)
+		require.False(t, found)
+	}
+}
+
+func TestUserGetAll(t *testing.T) {
+	keeper, ctx := keepertest.GitopiaKeeper(t)
+	items := createNUser(keeper, ctx, 10)
+	require.ElementsMatch(t, items, keeper.GetAllUser(ctx))
+}
+
+func TestUserCount(t *testing.T) {
+	keeper, ctx := keepertest.GitopiaKeeper(t)
+	items := createNUser(keeper, ctx, 10)
+	count := uint64(len(items))
+	require.Equal(t, count, keeper.GetUserCount(ctx))
+}
