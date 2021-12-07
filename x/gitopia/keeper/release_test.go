67ad9ee0b7da25f4eda5586e19c070a66f4acafc
Hariom Verma <hariom@gitopia.org>
2021-12-08 03:46:19 +0530

    Test Release Keeper

--- x/gitopia/keeper/release_test.go
+++ x/gitopia/keeper/release_test.go
@@ -1,56 +1,52 @@
-package keeper
+package keeper_test
 
 import (
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	keepertest "github.com/gitopia/gitopia/testutil/keeper"
+	"github.com/gitopia/gitopia/x/gitopia/keeper"
 	"github.com/gitopia/gitopia/x/gitopia/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func createNRelease(keeper *Keeper, ctx sdk.Context, n int) []types.Release {
+func createNRelease(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.Release {
 	items := make([]types.Release, n)
 	for i := range items {
-		items[i].Creator = "any"
 		items[i].Id = keeper.AppendRelease(ctx, items[i])
 	}
 	return items
 }
 
 func TestReleaseGet(t *testing.T) {
-	keeper, ctx := setupKeeper(t)
+	keeper, ctx := keepertest.GitopiaKeeper(t)
 	items := createNRelease(keeper, ctx, 10)
 	for _, item := range items {
-		assert.Equal(t, item, keeper.GetRelease(ctx, item.Id))
-	}
-}
-
-func TestReleaseExist(t *testing.T) {
-	keeper, ctx := setupKeeper(t)
-	items := createNRelease(keeper, ctx, 10)
-	for _, item := range items {
-		assert.True(t, keeper.HasRelease(ctx, item.Id))
+		got, found := keeper.GetRelease(ctx, item.Id)
+		require.True(t, found)
+		require.Equal(t, item, got)
 	}
 }
 
 func TestReleaseRemove(t *testing.T) {
-	keeper, ctx := setupKeeper(t)
+	keeper, ctx := keepertest.GitopiaKeeper(t)
 	items := createNRelease(keeper, ctx, 10)
 	for _, item := range items {
 		keeper.RemoveRelease(ctx, item.Id)
-		assert.False(t, keeper.HasRelease(ctx, item.Id))
+		_, found := keeper.GetRelease(ctx, item.Id)
+		require.False(t, found)
 	}
 }
 
 func TestReleaseGetAll(t *testing.T) {
-	keeper, ctx := setupKeeper(t)
+	keeper, ctx := keepertest.GitopiaKeeper(t)
 	items := createNRelease(keeper, ctx, 10)
-	assert.Equal(t, items, keeper.GetAllRelease(ctx))
+	require.ElementsMatch(t, items, keeper.GetAllRelease(ctx))
 }
 
 func TestReleaseCount(t *testing.T) {
-	keeper, ctx := setupKeeper(t)
+	keeper, ctx := keepertest.GitopiaKeeper(t)
 	items := createNRelease(keeper, ctx, 10)
 	count := uint64(len(items))
-	assert.Equal(t, count, keeper.GetReleaseCount(ctx))
+	require.Equal(t, count, keeper.GetReleaseCount(ctx))
 }
