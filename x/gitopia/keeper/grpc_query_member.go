b906dc40cf672a68f4e009ee533fb209e5127033
Hariom Verma <hariom@gitopia.org>
2022-08-18 17:05:29 +0530

    Dao member grpc queries

---
+++ x/gitopia/keeper/grpc_query_member.go
@@ -0,0 +1,98 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/gitopia/gitopia/x/gitopia/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) MemberAll(c context.Context, req *types.QueryAllMemberRequest) (*types.QueryAllMemberResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	var members []types.Member
+	ctx := sdk.UnwrapSDKContext(c)
+
+	store := ctx.KVStore(k.storeKey)
+	memberStore := prefix.NewStore(store, types.KeyPrefix(types.MemberKey))
+
+	pageRes, err := query.Paginate(memberStore, req.Pagination, func(key []byte, value []byte) error {
+		var member types.Member
+		if err := k.cdc.Unmarshal(value, &member); err != nil {
+			return err
+		}
+
+		members = append(members, member)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllMemberResponse{Member: members, Pagination: pageRes}, nil
+}
+
+func (k Keeper) DaoMemberAll(c context.Context, req *types.QueryAllDaoMemberRequest) (*types.QueryAllDaoMemberResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	var members []types.Member
+	ctx := sdk.UnwrapSDKContext(c)
+
+	daoAddress, err := k.ResolveAddress(ctx, req.DaoId)
+	if err != nil {
+		return nil, err
+	}
+
+	store := ctx.KVStore(k.storeKey)
+	memberStore := prefix.NewStore(store, []byte(types.GetMemberKeyForDaoAddress(daoAddress.address)))
+
+	pageRes, err := query.Paginate(memberStore, req.Pagination, func(key []byte, value []byte) error {
+		var member types.Member
+		if err := k.cdc.Unmarshal(value, &member); err != nil {
+			return err
+		}
+
+		members = append(members, member)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllDaoMemberResponse{Member: members, Pagination: pageRes}, nil
+}
+
+func (k Keeper) DaoMember(c context.Context, req *types.QueryGetDaoMemberRequest) (*types.QueryGetDaoMemberResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+
+	daoAddress, err := k.ResolveAddress(ctx, req.DaoId)
+	if err != nil {
+		return nil, err
+	}
+	userAddress, err := k.ResolveAddress(ctx, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	member, found := k.GetDaoMember(ctx, daoAddress.address, userAddress.address)
+	if !found {
+		return nil, sdkerrors.ErrKeyNotFound
+	}
+
+	return &types.QueryGetDaoMemberResponse{Member: member}, nil
+}
---
+++ x/gitopia/keeper/grpc_query_member_test.go
@@ -0,0 +1,117 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	keepertest "github.com/gitopia/gitopia/testutil/keeper"
+	"github.com/gitopia/gitopia/testutil/nullify"
+	"github.com/gitopia/gitopia/x/gitopia/types"
+)
+
+func TestMemberQuerySingle(t *testing.T) {
+	keeper, ctx := keepertest.GitopiaKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	msgs := createNMember(keeper, ctx, 2)
+	for _, tc := range []struct {
+		desc     string
+		request  *types.QueryGetMemberRequest
+		response *types.QueryGetMemberResponse
+		err      error
+	}{
+		{
+			desc:     "First",
+			request:  &types.QueryGetMemberRequest{Id: msgs[0].Id},
+			response: &types.QueryGetMemberResponse{Member: msgs[0]},
+		},
+		{
+			desc:     "Second",
+			request:  &types.QueryGetMemberRequest{Id: msgs[1].Id},
+			response: &types.QueryGetMemberResponse{Member: msgs[1]},
+		},
+		{
+			desc:    "KeyNotFound",
+			request: &types.QueryGetMemberRequest{Id: uint64(len(msgs))},
+			err:     sdkerrors.ErrKeyNotFound,
+		},
+		{
+			desc: "InvalidRequest",
+			err:  status.Error(codes.InvalidArgument, "invalid request"),
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			response, err := keeper.Member(wctx, tc.request)
+			if tc.err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t,
+					nullify.Fill(tc.response),
+					nullify.Fill(response),
+				)
+			}
+		})
+	}
+}
+
+func TestMemberQueryPaginated(t *testing.T) {
+	keeper, ctx := keepertest.GitopiaKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	msgs := createNMember(keeper, ctx, 5)
+
+	request := func(next []byte, offset, limit uint64, total bool) *types.QueryAllMemberRequest {
+		return &types.QueryAllMemberRequest{
+			Pagination: &query.PageRequest{
+				Key:        next,
+				Offset:     offset,
+				Limit:      limit,
+				CountTotal: total,
+			},
+		}
+	}
+	t.Run("ByOffset", func(t *testing.T) {
+		step := 2
+		for i := 0; i < len(msgs); i += step {
+			resp, err := keeper.MemberAll(wctx, request(nil, uint64(i), uint64(step), false))
+			require.NoError(t, err)
+			require.LessOrEqual(t, len(resp.Member), step)
+			require.Subset(t,
+				nullify.Fill(msgs),
+				nullify.Fill(resp.Member),
+			)
+		}
+	})
+	t.Run("ByKey", func(t *testing.T) {
+		step := 2
+		var next []byte
+		for i := 0; i < len(msgs); i += step {
+			resp, err := keeper.MemberAll(wctx, request(next, 0, uint64(step), false))
+			require.NoError(t, err)
+			require.LessOrEqual(t, len(resp.Member), step)
+			require.Subset(t,
+				nullify.Fill(msgs),
+				nullify.Fill(resp.Member),
+			)
+			next = resp.Pagination.NextKey
+		}
+	})
+	t.Run("Total", func(t *testing.T) {
+		resp, err := keeper.MemberAll(wctx, request(nil, 0, 0, true))
+		require.NoError(t, err)
+		require.Equal(t, len(msgs), int(resp.Pagination.Total))
+		require.ElementsMatch(t,
+			nullify.Fill(msgs),
+			nullify.Fill(resp.Member),
+		)
+	})
+	t.Run("InvalidRequest", func(t *testing.T) {
+		_, err := keeper.MemberAll(wctx, nil)
+		require.ErrorIs(t, err, status.Error(codes.InvalidArgument, "invalid request"))
+	})
+}
