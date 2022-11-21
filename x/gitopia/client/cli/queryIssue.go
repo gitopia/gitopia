11e08468f1c918b8b77ef61a87ccfc64c62b256e
Hariom Verma <hariom18599@gmail.com>
2022-11-22 02:58:44 +0530

    Fixed cli queries for issues

--- x/gitopia/client/cli/queryIssue.go
+++ x/gitopia/client/cli/queryIssue.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/gitopia/gitopia/x/gitopia/types"
+	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 )
 
@@ -43,26 +44,81 @@ func CmdListIssue() *cobra.Command {
 	return cmd
 }
 
-func CmdShowIssue() *cobra.Command {
+func CmdListRepositoryIssue() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "show-issue [id]",
-		Short: "shows a issue",
-		Args:  cobra.ExactArgs(1),
+		Use:   "list-repository-issue [id] [repository-name]",
+		Short: "list all repository issue",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			id, err := cast.ToStringE(args[0])
+			if err != nil {
+				return err
+			}
+			repositoryName, err := cast.ToStringE(args[1])
+			if err != nil {
+				return err
+			}
+
+			params := &types.QueryAllRepositoryIssueRequest{
+				Id:             id,
+				RepositoryName: repositoryName,
+				Pagination:     pageReq,
+			}
+
+			res, err := queryClient.RepositoryIssueAll(context.Background(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdShowRepositoryIssue() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-repository-issue [id] [repository-name] [issue-iid]",
+		Short: "shows a repository issue",
+		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx := client.GetClientContextFromCmd(cmd)
 
 			queryClient := types.NewQueryClient(clientCtx)
 
-			id, err := strconv.ParseUint(args[0], 10, 64)
+			id, err := cast.ToStringE(args[0])
+			if err != nil {
+				return err
+			}
+			repositoryName, err := cast.ToStringE(args[1])
+			if err != nil {
+				return err
+			}
+			issueIid, err := strconv.ParseUint(args[2], 10, 64)
 			if err != nil {
 				return err
 			}
 
-			params := &types.QueryGetIssueRequest{
-				Id: id,
+			params := &types.QueryGetRepositoryIssueRequest{
+				Id:             id,
+				RepositoryName: repositoryName,
+				IssueIid:       issueIid,
 			}
 
-			res, err := queryClient.Issue(context.Background(), params)
+			res, err := queryClient.RepositoryIssue(context.Background(), params)
 			if err != nil {
 				return err
 			}
