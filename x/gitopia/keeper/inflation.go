d516f3bfd9d1581b0cea24c5503a26968686a460
janani anbarasan <janani.anbarasan@gmail.com>
2022-12-16 19:25:23 -0500

    implement custom inlfation function

--- x/gitopia/keeper/inflation.go
+++ x/gitopia/keeper/inflation.go
@@ -6,5 +6,11 @@ import (
 )
 
 func (k Keeper) InflationFn(ctx sdk.Context, minter minttypes.Minter, params minttypes.Params, bondedRatio sdk.Dec) sdk.Dec {
+	gitopiaParams := k.GetParams(ctx)
+	if ctx.BlockTime().After(gitopiaParams.NextInflationTime) {
+		minter.Inflation = minter.Inflation.Quo(sdk.NewDec(2))
+		gitopiaParams.NextInflationTime = gitopiaParams.NextInflationTime.AddDate(2, 0, 0)
+		k.SetParams(ctx, gitopiaParams)
+	}
 	return minter.NextInflationRate(params, bondedRatio)
 }
