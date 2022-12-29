652d4c7f7e6b6ad55877b4f3316e14269d04c04e
janani anbarasan <janani.anbarasan@gmail.com>
2022-12-29 15:07:57 -0600

    handle empty gitopia params

--- x/gitopia/keeper/abci.go
+++ x/gitopia/keeper/abci.go
@@ -12,27 +12,31 @@ func (k Keeper) BeginBlocker(ctx sdk.Context) {
 	feeCollector := k.accountKeeper.GetModuleAccount(ctx, k.feeCollectorName)
 	feesCollected := k.bankKeeper.GetAllBalances(ctx, feeCollector.GetAddress())
 
-	ecosystemCoins := feesCollected.MulInt(math.NewInt(gitopiaParams.DistributionProportions.EcosystemProportion.Proportion)).QuoInt(sdk.NewInt(100))
-	addr, err := sdk.AccAddressFromBech32(gitopiaParams.DistributionProportions.EcosystemProportion.Address)
-	if err != nil {
-		ctx.Logger().Error(fmt.Sprintf("bad address %v", err))
-		panic(err)
-	}
-	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, k.feeCollectorName, addr, ecosystemCoins)
-	if err != nil {
-		ctx.Logger().Error(fmt.Sprintf("error distributing ecosystem proportion %v", err))
-		panic(err)
+	if gitopiaParams.DistributionProportions.EcosystemProportion.Proportion > 0 {
+		ecosystemCoins := feesCollected.MulInt(math.NewInt(gitopiaParams.DistributionProportions.EcosystemProportion.Proportion)).QuoInt(sdk.NewInt(100))
+		addr, err := sdk.AccAddressFromBech32(gitopiaParams.DistributionProportions.EcosystemProportion.Address)
+		if err != nil {
+			ctx.Logger().Error(fmt.Sprintf("bad address %v", err))
+			panic(err)
+		}
+		err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, k.feeCollectorName, addr, ecosystemCoins)
+		if err != nil {
+			ctx.Logger().Error(fmt.Sprintf("error distributing ecosystem proportion %v", err))
+			panic(err)
+		}
 	}
 
-	teamCoins := feesCollected.MulInt(math.NewInt(gitopiaParams.DistributionProportions.TeamProportion.Proportion)).QuoInt(sdk.NewInt(100))
-	addr, err = sdk.AccAddressFromBech32(gitopiaParams.DistributionProportions.TeamProportion.Address)
-	if err != nil {
-		ctx.Logger().Error(fmt.Sprintf("bad address %v", err))
-		panic(err)
-	}
-	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, k.feeCollectorName, addr, teamCoins)
-	if err != nil {
-		ctx.Logger().Error(fmt.Sprintf("error distributing team proportion %v", err))
-		panic(err)
+	if gitopiaParams.DistributionProportions.TeamProportion.Proportion > 0 {
+		teamCoins := feesCollected.MulInt(math.NewInt(gitopiaParams.DistributionProportions.TeamProportion.Proportion)).QuoInt(sdk.NewInt(100))
+		addr, err := sdk.AccAddressFromBech32(gitopiaParams.DistributionProportions.TeamProportion.Address)
+		if err != nil {
+			ctx.Logger().Error(fmt.Sprintf("bad address %v", err))
+			panic(err)
+		}
+		err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, k.feeCollectorName, addr, teamCoins)
+		if err != nil {
+			ctx.Logger().Error(fmt.Sprintf("error distributing team proportion %v", err))
+			panic(err)
+		}
 	}
 }
--- x/gitopia/keeper/inflation.go
+++ x/gitopia/keeper/inflation.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 )
@@ -14,8 +12,8 @@ const (
 func (k Keeper) InflationFn(ctx sdk.Context, minter minttypes.Minter, params minttypes.Params, bondedRatio sdk.Dec) sdk.Dec {
 	gitopiaParams := k.GetParams(ctx)
 
-	fmt.Println("heelo")
-	if ctx.BlockTime().After(gitopiaParams.NextInflationTime) {
+	if !gitopiaParams.NextInflationTime.IsZero() &&
+		ctx.BlockTime().After(gitopiaParams.NextInflationTime) {
 		minter.Inflation = minter.Inflation.Quo(sdk.NewDec(2))
 		params.InflationMax = params.InflationMax.Quo(sdk.NewDec(2))
 		params.InflationMin = params.InflationMin.Quo(sdk.NewDec(2))
@@ -26,7 +24,7 @@ func (k Keeper) InflationFn(ctx sdk.Context, minter minttypes.Minter, params min
 
 	// TODO: stop minting after max total supply is reached
 	// if k.mintKeeper.StakingTokenSupply(ctx).GTE(math.NewInt(MAX_TOTAL_SUPPLY)) {
-		
+
 	// }
-	return minter.NextInflationRate(params, bondedRatio) 
+	return minter.NextInflationRate(params, bondedRatio)
 }
