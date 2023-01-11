1d6b99d90063b4c610755c4e3389abf26490a520
janani anbarasan <janani.anbarasan@gmail.com>
2023-01-10 21:40:08 -0600

    handle zero inflation

--- x/gitopia/keeper/inflation.go
+++ x/gitopia/keeper/inflation.go
@@ -10,12 +10,21 @@ func (k Keeper) InflationFn(ctx sdk.Context, minter minttypes.Minter, params min
 
 	if !gitopiaParams.NextInflationTime.IsZero() &&
 		ctx.BlockTime().After(gitopiaParams.NextInflationTime) {
-		minter.Inflation = minter.Inflation.Quo(sdk.NewDec(2))
+		// update inflation params
 		params.InflationMax = params.InflationMax.Quo(sdk.NewDec(2))
 		params.InflationMin = params.InflationMin.Quo(sdk.NewDec(2))
 		k.mintKeeper.SetParams(ctx, params)
+
 		gitopiaParams.NextInflationTime = gitopiaParams.NextInflationTime.AddDate(2, 0, 0)
 		k.SetParams(ctx, gitopiaParams)
+
+		minter.Inflation = minter.Inflation.Quo(sdk.NewDec(2))
+	}
+
+	// minting stops.
+	// just being defensive. zero inflation is gracefully handled by the mint module
+	if minter.Inflation.IsZero() {
+		return minter.Inflation
 	}
 
 	return minter.NextInflationRate(params, bondedRatio)
