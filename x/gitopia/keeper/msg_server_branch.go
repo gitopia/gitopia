f7de6b9123b6f5287ca91506d3ef74dae0bf4db6
janani anbarasan <janani.anbarasan@gmail.com>
2023-01-21 00:50:19 -0600

    implement toggle force-push transaction

--- x/gitopia/keeper/msg_server_branch.go
+++ x/gitopia/keeper/msg_server_branch.go
@@ -307,11 +307,39 @@ func (k msgServer) MultiDeleteBranch(goCtx context.Context, msg *types.MsgMultiD
 	return &types.MsgMultiDeleteBranchResponse{}, nil
 }
 
-func (k msgServer) ToggleForcePush(goCtx context.Context,  msg *types.MsgToggleForcePush) (*types.MsgToggleForcePushResponse, error) {
+func (k msgServer) ToggleForcePush(goCtx context.Context, msg *types.MsgToggleForcePush) (*types.MsgToggleForcePushResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-    // TODO: Handling the message
-    _ = ctx
+	_, found := k.GetUser(ctx, msg.Creator)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("creator (%v) doesn't exist", msg.Creator))
+	}
+
+	address, err := k.ResolveAddress(ctx, msg.RepositoryId.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	repository, found := k.GetAddressRepository(ctx, address.address, msg.RepositoryId.Name)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("repository (%v/%v) doesn't exist", msg.RepositoryId.Id, msg.RepositoryId.Name))
+	}
+
+	if !k.HavePermission(ctx, msg.Creator, repository, types.ToggleForcePushToBranchPermission) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, fmt.Sprintf("user (%v) doesn't have permission to perform this operation", msg.Creator))
+	}
+
+	branch, found := k.GetRepositoryBranch(ctx, repository.Id, msg.BranchName)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("branch (%v) doesn't exist", msg.BranchName))
+	}
+
+	branch.AllowForcePush = !branch.AllowForcePush
+	branch.UpdatedAt = ctx.BlockTime().Unix()
+	k.SetRepositoryBranch(ctx, branch)
+
+	repository.UpdatedAt = ctx.BlockTime().Unix()
+	k.SetRepository(ctx, repository)
 
 	return &types.MsgToggleForcePushResponse{}, nil
 }
