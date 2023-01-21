fd2a905c163e8d8d8d4a6691f5409980b44c4f40
janani anbarasan <janani.anbarasan@gmail.com>
2023-01-21 00:24:18 -0600

    cleanup files

--- x/gitopia/client/cli/tx_branch.go
+++ x/gitopia/client/cli/tx_branch.go
@@ -1,12 +1,16 @@
 package cli
 
 import (
+	"strconv"
+	
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/gitopia/gitopia/x/gitopia/types"
 	"github.com/spf13/cobra"
 )
+
+var _ = strconv.Itoa(0)
 
 func CmdSetBranch() *cobra.Command {
 	cmd := &cobra.Command{
@@ -104,4 +108,38 @@ func CmdDeleteBranch() *cobra.Command {
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
+}
+
+func CmdToggleForcePush() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "toggle-force-push [id] [repository-id] [branch-name]",
+		Short: "configure restricted push access to branch",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+      		 argId := args[0]
+             argRepositoryId := args[1]
+             argBranchName := args[2]
+            
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgToggleForcePush(
+				clientCtx.GetFromAddress().String(),
+				argId,
+				argRepositoryId,
+				argBranchName,
+				
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+    return cmd
 }
