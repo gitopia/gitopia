4a489ed19a9595b29e71872533ed917cfa44e047
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-25 17:09:08 -0500

    fix testutil

--- testutil/keeper/rewards.go
+++ testutil/keeper/rewards.go
@@ -8,22 +8,27 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/gitopia/gitopia/x/rewards/keeper"
-	"github.com/gitopia/gitopia/x/rewards/types"
+	gitopiakeeper "github.com/gitopia/gitopia/x/gitopia/keeper"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
 
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	authzkeeper "github.com/cosmos/cosmos-sdk/x/authz/keeper"
 	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
-	gitopiakeeper "github.com/gitopia/gitopia/x/gitopia/keeper"
+	"github.com/gitopia/gitopia/x/gitopia/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	mintkeeper "github.com/cosmos/cosmos-sdk/x/mint/keeper"
 )
 
 func RewardsKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
+	logger := log.NewNopLogger()
 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
 	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
 
@@ -33,21 +38,17 @@ func RewardsKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 	stateStore.MountStoreWithDB(memStoreKey, storetypes.StoreTypeMemory, nil)
 	require.NoError(t, stateStore.LoadLatestVersion())
 
+	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, logger)
+
 	registry := codectypes.NewInterfaceRegistry()
 	cdc := codec.NewProtoCodec(registry)
 
+	amino := codec.NewLegacyAmino()
 	ss := typesparams.NewSubspace(cdc,
-		types.Amino,
+		amino,
 		storeKey,
 		memStoreKey,
 		"GitopiaSubSpace",
-	)
-
-	paramsSubspace := typesparams.NewSubspace(cdc,
-		types.Amino,
-		storeKey,
-		memStoreKey,
-		"RewardsParams",
 	)
 
 	ak := authkeeper.NewAccountKeeper(
@@ -66,34 +67,54 @@ func RewardsKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		ak,
 	)
 
+	bankParamsSubspace := typesparams.NewSubspace(cdc,
+		amino,
+		storeKey,
+		memStoreKey,
+		"bank",
+	)
+	bankKeeper := bankkeeper.NewBaseKeeper(
+		cdc,
+		storeKey,
+		ak,
+		bankParamsSubspace,
+		nil,
+	)
+	bankKeeper.SetParams(ctx, banktypes.DefaultParams())
+
+	stakingKeeper := stakingkeeper.NewKeeper(
+		cdc,
+		storeKey,
+		ak,
+		bankKeeper,
+		ss,
+	)
+
+	mintKeeper := mintkeeper.NewKeeper(
+		cdc, storeKey, ss, stakingKeeper, ak,
+		bankKeeper, types.MinterAccountName)
+
 	gitopiaKeeper := gitopiakeeper.NewKeeper(
 		codec.NewProtoCodec(registry),
 		storeKey,
 		memStoreKey,
+		types.MinterAccountName,
+		authtypes.FeeCollectorName,
 		ak,
 		&authzKeeper,
-	)
-
-	// TODO: no tests written for code using this dependency. initialize with correct params
-	stakingKeeper := stakingkeeper.NewKeeper(
-		cdc,
-		storeKey,
-		nil,
-		nil,
-		ss,
-	)
+		bankKeeper,
+		mintKeeper,
+	) 
 
 	k := keeper.NewKeeper(
 		cdc,
 		storeKey,
 		memStoreKey,
-		paramsSubspace,
 		gitopiaKeeper,
 		stakingKeeper,
 		govkeeper.Keeper{},
 	)
 
-	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
 
 	// Initialize params
 
