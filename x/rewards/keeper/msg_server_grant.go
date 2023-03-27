ad9c1052592bddd19e0a010639108289f4daf557
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-26 23:36:10 -0500

    allow only airdrop address to grant rewards

--- x/rewards/keeper/msg_server_grant.go
+++ x/rewards/keeper/msg_server_grant.go
@@ -3,26 +3,30 @@ package keeper
 import (
 	"context"
 
-    "github.com/gitopia/gitopia/x/rewards/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/gitopia/gitopia/x/rewards/types"
 )
 
-
-func (k msgServer) Grant(goCtx context.Context,  msg *types.MsgGrant) (*types.MsgGrantResponse, error) {
+func (k msgServer) Grant(goCtx context.Context, msg *types.MsgGrant) (*types.MsgGrantResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	params := k.GetParams(ctx)
+	if msg.Creator != params.AirdropAddress {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "bad airdrop address "+msg.Creator)
+	}
 
 	fromAddr, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "bad address " + msg.Creator)
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "bad address "+msg.Creator)
 	}
 
 	toAddr, err := sdk.AccAddressFromBech32(msg.To)
 	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "bad address " + msg.To)
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "bad address "+msg.To)
 	}
 
-    err = k.bankKeeper.SendCoins(ctx, fromAddr, toAddr, msg.Amount)
+	err = k.bankKeeper.SendCoins(ctx, fromAddr, toAddr, msg.Amount)
 	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "error transfering reward")
 	}
