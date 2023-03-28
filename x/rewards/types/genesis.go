dd89a5d2afa8bde8a1a96cdb630c4b5756c84d8f
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-27 19:56:39 -0500

    add ignite placeholders

--- x/rewards/types/genesis.go
+++ x/rewards/types/genesis.go
@@ -1,21 +1,23 @@
 package types
 
+// this line is used by starport scaffolding # genesis/types/import
+
 // DefaultIndex is the default global index
 const DefaultIndex uint64 = 1
 
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-// this line is used by starport scaffolding # genesis/types/default
+		// this line is used by starport scaffolding # genesis/types/default
 	}
 }
 
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
-    // Check for duplicated index in rewards
+	// Check for duplicated index in rewards
 
-// this line is used by starport scaffolding # genesis/types/validate
+	// this line is used by starport scaffolding # genesis/types/validate
 
 	return nil
 }
