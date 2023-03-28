9988883f126809a44cf629c835abfb9fa3467128
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-27 19:41:18 -0500

    allow only whitelisted address to grant rewards

--- x/rewards/keeper/msg_server_grant.go
+++ x/rewards/keeper/msg_server_grant.go
@@ -5,14 +5,21 @@ import (
 
     "github.com/gitopia/gitopia/x/rewards/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 
 func (k msgServer) Grant(goCtx context.Context,  msg *types.MsgGrant) (*types.MsgGrantResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-    // TODO: Handling the message
-    _ = ctx
+    params := k.GetParams(ctx)
+
+	if params.EvaluatorAddress != msg.Creator {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "user (%v) doesn't have permission to perform this operation", msg.Creator)
+	}
+
+	
+
 
 	return &types.MsgGrantResponse{}, nil
 }
