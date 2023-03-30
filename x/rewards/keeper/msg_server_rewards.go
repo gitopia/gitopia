1cc3539b673b47f0da911015a8b489f71642c31e
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-30 01:55:22 -0500

    reward only available pool amount

--- x/rewards/keeper/msg_server_rewards.go
+++ x/rewards/keeper/msg_server_rewards.go
@@ -8,6 +8,29 @@ import (
 	"github.com/gitopia/gitopia/x/rewards/types"
 )
 
+const (
+	SERIES_ONE = 1
+)
+
+func getSeries(rss []*types.RewardSeries, series int32) (*types.RewardSeries, error) {
+	for _, rs := range rss {
+		if rs.Series == series {
+			return rs, nil
+		}
+	}
+	return nil, sdkerrors.Wrapf(sdkerrors.ErrLogic, "series %d reward pool not found", series)
+}
+
+func addSeries(rss []*types.RewardSeries, series int32, s *types.RewardSeries) ([]*types.RewardSeries, error) {
+	for _, rs := range rss {
+		if rs.Series == series {
+			*rs = *s
+			return rss, nil
+		}
+	}
+	return nil, sdkerrors.Wrapf(sdkerrors.ErrLogic, "series %d reward pool not found", series)
+}
+
 func (k msgServer) CreateReward(goCtx context.Context, msg *types.MsgCreateReward) (*types.MsgCreateRewardResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
@@ -15,6 +38,21 @@ func (k msgServer) CreateReward(goCtx context.Context, msg *types.MsgCreateRewar
 
 	if params.EvaluatorAddress != msg.Creator {
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "user (%v) doesn't have permission to perform this operation", msg.Creator)
+	}
+
+	rs, err := getSeries(params.RewardSeries, SERIES_ONE)
+	if err != nil {
+		return nil, err
+	}
+
+	if rs.RewardPoolClaimed.IsEqual(rs.RewardPool) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "series 1 reward pool claimed")
+	}
+
+	availablePoolBal := rs.RewardPool.Sub(rs.RewardPoolClaimed...)
+	amount := msg.Amount
+	if availablePoolBal.IsAllLT(msg.Amount) {
+		amount = availablePoolBal
 	}
 
 	// Check if the value already exists
@@ -29,12 +67,22 @@ func (k msgServer) CreateReward(goCtx context.Context, msg *types.MsgCreateRewar
 	var reward = types.Reward{
 		Creator:   msg.Creator,
 		Recipient: msg.Recipient,
-		Amount:    msg.Amount,
+		Amount:    amount,
 	}
 
 	k.SetReward(
 		ctx,
 		reward,
 	)
-	return &types.MsgCreateRewardResponse{}, nil
+
+	rs.RewardPoolClaimed = rs.RewardPoolClaimed.Add(amount...)
+	params.RewardSeries, err = addSeries(params.RewardSeries, SERIES_ONE, rs)
+	if err != nil {
+		return nil, err
+	}
+	k.SetParams(ctx, params)
+
+	return &types.MsgCreateRewardResponse{
+		Amount: amount,
+	}, nil
 }
