4a68ee09b0fe483335ea17150312ba64109d7661
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-31 11:20:59 -0500

    refactor reward pool expiry

--- x/rewards/keeper/abci.go
+++ x/rewards/keeper/abci.go
@@ -9,74 +9,53 @@ import (
 func (k Keeper) BeginBlocker(ctx sdk.Context) {
 	params := k.GetParams(ctx)
 
-	if !params.RewardSeries.SeriesOne.Expiry.IsZero() &&
-		params.RewardSeries.SeriesOne.Expiry.Before(ctx.BlockTime()) {
-		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(types.RewardsSeriesOneAccount)
+	err := k.TransferRewardPoolBalance(ctx, params.RewardSeries.SeriesOne, types.RewardsSeriesOneAccount)
+	if err != nil {
+		panic(err)
+	}
+
+	err = k.TransferRewardPoolBalance(ctx, params.RewardSeries.SeriesTwo, types.RewardsSeriesTwoAccount)
+	if err != nil {
+		panic(err)
+	}
+
+	err = k.TransferRewardPoolBalance(ctx, params.RewardSeries.SeriesThree, types.RewardsSeriesThreeAccount)
+	if err != nil {
+		panic(err)
+	}
+
+	err = k.TransferRewardPoolBalance(ctx, params.RewardSeries.SeriesFour, types.RewardsSeriesFourAccount)
+	if err != nil {
+		panic(err)
+	}
+
+	err = k.TransferRewardPoolBalance(ctx, params.RewardSeries.SeriesFive, types.RewardsSeriesFiveAccount)
+	if err != nil {
+		panic(err)
+	}
+
+	err = k.TransferRewardPoolBalance(ctx, params.RewardSeries.SeriesSix, types.RewardsSeriesSixAccount)
+	if err != nil {
+		panic(err)
+	}
+
+	err = k.TransferRewardPoolBalance(ctx, params.RewardSeries.SeriesSeven, types.RewardsSeriesSevenAccount)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (k Keeper) TransferRewardPoolBalance(ctx sdk.Context, rewardPool *types.RewardPool, moduleAcc string) error {
+	if !rewardPool.Expiry.IsZero() && rewardPool.Expiry.Before(ctx.BlockTime()) {
+		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(moduleAcc)
 		rewardsModuleBalance := k.bankKeeper.GetAllBalances(ctx, rewardsModuleAddress)
 
 		if !rewardsModuleBalance.Empty() {
-			k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardsSeriesOneAccount, disttypes.ModuleName, rewardsModuleBalance)
+			err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, moduleAcc, disttypes.ModuleName, rewardsModuleBalance)
+			if err != nil {
+				return err
+			}
 		}
 	}
-
-	if !params.RewardSeries.SeriesTwo.Expiry.IsZero() &&
-		params.RewardSeries.SeriesTwo.Expiry.Before(ctx.BlockTime()) {
-		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(types.RewardsSeriesTwoAccount)
-		rewardsModuleBalance := k.bankKeeper.GetAllBalances(ctx, rewardsModuleAddress)
-
-		if !rewardsModuleBalance.Empty() {
-			k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardsSeriesTwoAccount, disttypes.ModuleName, rewardsModuleBalance)
-		}
-	}
-
-	if !params.RewardSeries.SeriesThree.Expiry.IsZero() &&
-		params.RewardSeries.SeriesThree.Expiry.Before(ctx.BlockTime()) {
-		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(types.RewardsSeriesThreeAccount)
-		rewardsModuleBalance := k.bankKeeper.GetAllBalances(ctx, rewardsModuleAddress)
-
-		if !rewardsModuleBalance.Empty() {
-			k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardsSeriesThreeAccount, disttypes.ModuleName, rewardsModuleBalance)
-		}
-	}
-
-	if !params.RewardSeries.SeriesFour.Expiry.IsZero() &&
-		params.RewardSeries.SeriesFour.Expiry.Before(ctx.BlockTime()) {
-		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(types.RewardsSeriesFourAccount)
-		rewardsModuleBalance := k.bankKeeper.GetAllBalances(ctx, rewardsModuleAddress)
-
-		if !rewardsModuleBalance.Empty() {
-			k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardsSeriesFourAccount, disttypes.ModuleName, rewardsModuleBalance)
-		}
-	}
-
-	if !params.RewardSeries.SeriesFive.Expiry.IsZero() &&
-		params.RewardSeries.SeriesFive.Expiry.Before(ctx.BlockTime()) {
-		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(types.RewardsSeriesFiveAccount)
-		rewardsModuleBalance := k.bankKeeper.GetAllBalances(ctx, rewardsModuleAddress)
-
-		if !rewardsModuleBalance.Empty() {
-			k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardsSeriesFiveAccount, disttypes.ModuleName, rewardsModuleBalance)
-		}
-	}
-
-	if !params.RewardSeries.SeriesSix.Expiry.IsZero() &&
-		params.RewardSeries.SeriesSix.Expiry.Before(ctx.BlockTime()) {
-		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(types.RewardsSeriesSixAccount)
-		rewardsModuleBalance := k.bankKeeper.GetAllBalances(ctx, rewardsModuleAddress)
-
-		if !rewardsModuleBalance.Empty() {
-			k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardsSeriesSixAccount, disttypes.ModuleName, rewardsModuleBalance)
-		}
-	}
-
-	if !params.RewardSeries.SeriesSeven.Expiry.IsZero() &&
-		params.RewardSeries.SeriesSeven.Expiry.Before(ctx.BlockTime()) {
-		rewardsModuleAddress := k.accountKeeper.GetModuleAddress(types.RewardsSeriesSevenAccount)
-		rewardsModuleBalance := k.bankKeeper.GetAllBalances(ctx, rewardsModuleAddress)
-
-		if !rewardsModuleBalance.Empty() {
-			k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.RewardsSeriesSevenAccount, disttypes.ModuleName, rewardsModuleBalance)
-		}
-	}
-
+	return nil
 }
