89821dedafadabe1adc4392246870ca25a3e9039
janani anbarasan <janani.anbarasan@gmail.com>
2023-04-12 13:08:01 +0530

    [refactor] code org

--- x/gitopia/keeper/grpc_query_team_vesting.go
+++ x/gitopia/keeper/grpc_query_team_vesting.go
@@ -5,39 +5,9 @@ import (
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/gitopia/gitopia/app/params"
 	"github.com/gitopia/gitopia/x/gitopia/types"
 )
-
-func (k Keeper) GetVestedAmount(ctx sdk.Context, address string) (sdk.Coin, error) {
-	gitopiaParams := k.GetParams(ctx)
-
-	var proportion sdk.Dec
-	for _, p := range gitopiaParams.TeamProportions {
-		if address == p.Address {
-			proportion = p.Proportion
-			break
-		}
-	}
-
-	if proportion.Equal(sdk.NewDec(0)) {
-		return sdk.Coin{}, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "account (%v) doesn't have permission to perform this operation", address)
-	}
-
-	vested := vestedTeamTokens(gitopiaParams.GenesisTime, ctx.BlockTime())
-	if vested.Amount.IsZero() {
-		return sdk.Coin{}, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "team tokens have not vested")
-	}
-
-	dec := sdk.NewDec(vested.Amount.Int64())
-	vestedProportion := sdk.Coin{
-		Amount: dec.Mul(proportion).Quo(sdk.NewDec(100)).TruncateInt(),
-		Denom:  vested.Denom,
-	}
-
-	return vestedProportion, nil
-}
 
 func (k Keeper) VestedAmount(c context.Context, req *types.QueryVestedAmountRequest) (*types.QueryVestedAmountResponse, error) {
 	ctx := sdk.UnwrapSDKContext(c)
--- x/gitopia/keeper/msg_server_exercise.go
+++ x/gitopia/keeper/msg_server_exercise.go
@@ -2,51 +2,23 @@ package keeper
 
 import (
 	"context"
-	"time"
 
-	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/gitopia/gitopia/app/params"
 	"github.com/gitopia/gitopia/x/gitopia/types"
 	"github.com/pkg/errors"
 )
 
-const (
-	TEAM_VESTING_AMOUNT = 339_118_201_000_000 // max team supply
-	CLIFF_PERIOD        = 12                  // 1 year cliff
-	VESTING_PERIOD      = 120                 // 10 years * 12 months
-)
-
-func vestedTeamTokens(startTime, currentTime time.Time) sdk.Coin {
-	// Calculate the number of months between genesis time and current time
-	months := currentTime.Year()*12 + int(currentTime.Month()) - (startTime.Year()*12 + int(startTime.Month()))
-
-	if months <= CLIFF_PERIOD {
-		return sdk.NewCoin(params.BaseCoinUnit, math.NewInt(0)) // No vesting before the end of the cliff period
-	}
-
-	// Calculate the vested amount
-	vestedMonths := months - CLIFF_PERIOD
-	if vestedMonths > VESTING_PERIOD {
-		vestedMonths = VESTING_PERIOD
-	}
-
-	vestedAmount := TEAM_VESTING_AMOUNT / VESTING_PERIOD * vestedMonths
-
-	return sdk.NewCoin(params.BaseCoinUnit, math.NewInt(int64(vestedAmount)))
-}
-
 func (k msgServer) Exercise(goCtx context.Context, msg *types.MsgExercise) (*types.MsgExerciseResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	vestedProportion, err := k.GetVestedAmount(ctx, msg.Creator)
+	vestedAmount, err := k.GetVestedAmount(ctx, msg.Creator)
 	if err != nil {
 		return nil, err
 	}
 	exercisedAmount, f := k.GetExercisedAmount(ctx, msg.Creator)
 	amount := sdk.Coins{sdk.NormalizeCoin(msg.Amount)}
-	if amount.IsAllGT(sdk.Coins{vestedProportion}.Sub(exercisedAmount.Amount)) {
+	if amount.IsAllGT(sdk.Coins{vestedAmount}.Sub(exercisedAmount.Amount)) {
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "cannot exercise more than vested tokens")
 	}
 
