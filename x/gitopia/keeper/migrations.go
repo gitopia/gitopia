2552729e3a85acb991aaa4afb15bce78082944d4
janani anbarasan <janani.anbarasan@gmail.com>
2023-05-08 20:51:49 +0530

    rename testnet migration package

--- x/gitopia/keeper/migrations.go
+++ x/gitopia/keeper/migrations.go
@@ -1,10 +1,5 @@
 package keeper
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	v3 "github.com/gitopia/gitopia/x/gitopia/migrations/v3"
-)
-
 // Migrator is a struct for handling in-place store migrations.
 type Migrator struct {
 	keeper Keeper
@@ -13,9 +8,4 @@ type Migrator struct {
 // NewMigrator returns a new Migrator.
 func NewMigrator(keeper Keeper) Migrator {
 	return Migrator{keeper: keeper}
-}
-
-// Migrate2to3 migrates from version 2 to 3.
-func (m Migrator) Migrate2to3(ctx sdk.Context) error {
-	return v3.MigrateStore(ctx, m.keeper.storeKey, m.keeper.cdc)
 }
--- x/gitopia/module.go
+++ x/gitopia/module.go
@@ -133,8 +133,6 @@ func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sd
 // RegisterServices registers a GRPC query service to respond to the
 // module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
-	m := keeper.NewMigrator(am.keeper)
-	cfg.RegisterMigration(types.ModuleName, 2, m.Migrate2to3)
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
